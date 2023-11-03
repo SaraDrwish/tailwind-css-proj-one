78cabbafc686927a50aabdb4e3eb2e9045f33023
Sara <38770404+SaraDrwish@users.noreply.github.com>
2023-11-03 15:20:11 +0300

    56

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+
+      colors:
+      {
+        primary:"#fe5561",
+        secondary: {
+          100: "#Fe4548",
+          200:"#888883"
+        }
+      }
+      
+    },
+  },
+  plugins: [],
+}
+
